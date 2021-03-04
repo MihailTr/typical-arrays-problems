669d6967c9c417e7efa087df689a92a3d837581c
MihailTr <tmyfly@gmail.com>
2021-03-04 23:09:37 +0200

    feat: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined){
+        return 0;
+    }
+    if (array.length == 0){
+        return 0;
+    }
+    array.sort(function (a, b) {
+        return a - b;
+    });
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined){
+        return 0;
+    }
+    if (array.length == 0){
+        return 0;
+    }
+    array.sort(function (a, b) {
+        return a - b;
+    });
+    return array[array.length -1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined){
+        return 0;
+    }
+    if (array.length == 0){
+        return 0;
+    }
+    return array.reduce((x, y) => x + y, 0)/array.length;
 }
+
+// console.log(max([2,-24,13,-11,16,-12,-16,24,13,-38,19,-16,19,3,2,32,0,-10,-19,-27]));
+// console.log(max([]));
+// console.log(max());
